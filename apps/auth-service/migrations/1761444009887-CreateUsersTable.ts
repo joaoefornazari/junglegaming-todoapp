ebd813ee688e7c3bfd9f9c2c39d185c74134e452
joaoefornazari <joaoefornazari@gmail.com>
2025-10-27 14:24:57 -0300

    feat: migration to create users table

---
+++ apps/auth-service/migrations/1761444009887-CreateUsersTable.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1761444009887 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "username",
+                        type: "varchar",
+                        isUnique: true,
+                        isNullable: false
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isUnique: true,
+                        isNullable: false
+                    },
+                    {
+                        name: "password_hash",
+                        type: "varchar",
+                        isNullable: false
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP"
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        const table = await queryRunner.getTable("users");
+        if (table) {
+            await queryRunner.dropTable("users");
+        }
+    }
+
+}
