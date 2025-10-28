479da75445f311a07ddd3c90952665f2da82a733
joaoefornazari <joaoefornazari@gmail.com>
2025-10-28 14:39:17 -0300

    fix: fixing database name

--- apps/auth-service/typeorm.config.ts
+++ apps/auth-service/typeorm.config.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
   port: parseInt(process.env.DB_PORT ? process.env.DB_PORT : '5432'),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   entities: ['src/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
   synchronize: false, // Always false when using migrations
