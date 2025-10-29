44dd2fb3b738e81f2aaccd1fd5342e4da2030eea
joaoefornazari <joaoefornazari@gmail.com>
2025-10-28 22:39:08 -0300

    feat: adding user module and services to help better organize Auth service

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
       synchronize: false,
     }),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
